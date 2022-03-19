2720a85d08de9992b8c8fee70414b9940fb2f236
Dongbin <bnitech88@gmail.com>
2022-03-20 00:20:30 +0900

    [dongbin] #3.4.2.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import Counter from './Counter';
+import Say from './Say';
 
 function App() {
-  return <Counter />;
+  return <Say />;
 }
 
 export default App;
