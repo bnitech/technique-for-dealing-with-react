5b6a1ba7770318c3ee202541c265a2e26bf3f3f3
Dongbin <bnitech88@gmail.com>
2022-03-19 23:10:22 +0900

    [dongbin] #3.4.1.3

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -14,7 +14,10 @@ class Counter extends Component<any, { number; fixedNumber }> {
         <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
           onClick={() => {
-            this.setState({ number: number + 1 });
+            this.setState(prevState => {
+              return { number: prevState.number + 1 };
+            });
+            this.setState(prevState => ({ number: prevState.number + 1 }));
           }}
         >
           +1
