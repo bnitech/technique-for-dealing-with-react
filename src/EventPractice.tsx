6a191f074cf933334a47b94f89726a6237f0fb4b
Dongbin <bnitech88@gmail.com>
2022-03-20 01:32:10 +0900

    [dongbin] #4.2.3.1

--- src/EventPractice.tsx
+++ src/EventPractice.tsx
@@ -1,9 +1,28 @@
-import { Component } from 'react';
+import React, {Component, MouseEventHandler, SyntheticEvent} from 'react';
 
-class EventPractice extends Component<any, {message: string}> {
+class EventPractice extends Component<any, { message: string }> {
   state = {
     message: '',
   };
+
+  constructor(props: any) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e: any) {
+    this.setState({
+      message: e.target.value,
+    });
+  }
+
+  handleClick() {
+    alert(this.state.message);
+    this.setState({
+      message: '',
+    });
+  }
 
   render() {
     return (
@@ -13,22 +32,10 @@ class EventPractice extends Component<any, {message: string}> {
           type="text"
           name="message"
           placeholder="아무거나 입력해 보세요"
-          onChange={e => {
-            this.setState({
-              message: e.target.value,
-            });
-          }}
+          value={this.state.message}
+          onChange={this.handleChange}
         />
-        <button
-          onClick={() => {
-            alert(this.state.message);
-            this.setState({
-              message: '',
-            });
-          }}
-        >
-          확인
-        </button>
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
