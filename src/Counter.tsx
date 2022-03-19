331a20365306b8631c6530c94b93156e8014f1bf
Dongbin <bnitech88@gmail.com>
2022-03-19 23:01:48 +0900

    [dongbin] #3.4.1.1

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,18 +1,20 @@
 import { Component } from 'react';
 
-class Counter extends Component<any, {number}> {
+class Counter extends Component<any, {number, fixedNumber}> {
   constructor(props) {
     super(props);
     this.state = {
       number: 0,
+      fixedNumber: 0,
     };
   }
 
   render() {
-    const { number } = this.state;
+    const { number, fixedNumber } = this.state;
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
           onClick={() => {
             this.setState({ number: number + 1 });
