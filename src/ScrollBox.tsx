b495bf54897be9bdefb12e9c61c06ff0086b824f
Dongbin <bnitech88@gmail.com>
2022-03-22 20:59:34 +0900

    [dongbin] #5.3.3

--- src/ScrollBox.tsx
+++ src/ScrollBox.tsx
@@ -4,6 +4,13 @@ import CSS from 'csstype';
 class ScrollBox extends Component<any, any> {
   box: HTMLDivElement | null | undefined;
 
+  scrollToBottom = () => {
+    if (this.box) {
+      const { scrollHeight, clientHeight } = this.box;
+      this.box.scrollTop = scrollHeight - clientHeight;
+    }
+  };
+
   render() {
     const style: CSS.Properties = {
       border: '1px solid black',
