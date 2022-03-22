b68d090363a56d34000bb1ff92c9a5704a502525
Dongbin <bnitech88@gmail.com>
2022-03-22 20:43:43 +0900

    [dongbin] #5.3.2.1

---
+++ src/ScrollBox.tsx
@@ -0,0 +1,35 @@
+import { Component } from 'react';
+import CSS from 'csstype';
+
+class ScrollBox extends Component<any, any> {
+  box: HTMLDivElement | null | undefined;
+
+  render() {
+    const style: CSS.Properties = {
+      border: '1px solid black',
+      height: '300px',
+      width: '300px',
+      overflow: 'auto',
+      position: 'relative',
+    };
+
+    const innerStyle: CSS.Properties = {
+      width: '100%',
+      height: '650px',
+      background: 'linear-gradient(white, black)',
+    };
+
+    return (
+      <div
+        style={style}
+        ref={ref => {
+          this.box = ref;
+        }}
+      >
+        <div style={innerStyle} />
+      </div>
+    );
+  }
+}
+
+export default ScrollBox;
