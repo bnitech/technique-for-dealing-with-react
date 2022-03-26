f8c69c65bb24a679107fe9139c0354080251430a
Dongbin <bnitech88@gmail.com>
2022-03-26 23:32:15 +0900

    [dongbin] #8.3.2

--- src/Info.tsx
+++ src/Info.tsx
@@ -1,30 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useReducer } from 'react';
+
+function reducer(state: any, action: HTMLInputElement) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
 
 const Info = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
-  useEffect(() => {
-    console.log('effect');
-    console.log(name);
-    return () => {
-      console.log('cleanup');
-      console.log(name);
-    };
-  }, [name]);
-
-  const onChangeName = (e: any) => {
-    setName(e.target.value);
-  };
-
-  const onChangeNickname = (e: any) => {
-    setNickname(e.target.value);
+  const [state, dispatch] = useReducer(reducer, { name: '', nickname: '' });
+  const { name, nickname } = state;
+  const onChange = (e: any) => {
+    dispatch(e.target);
   };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
