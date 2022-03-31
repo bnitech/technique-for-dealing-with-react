18a68a04b539c5a12e92e44a39c791d4e2e3b74d
Dongbin <bnitech88@gmail.com>
2022-03-31 16:58:57 +0900

    [dongbin] #9.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import Average from './Average';
+import { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  return <Average />;
-};
+class App extends Component<any, any> {
+  render() {
+    return (
+      <div className="App">
+        \
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="nooperner noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
 
 export default App;
