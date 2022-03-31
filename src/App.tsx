d53cfa66b3c347256dc56f6f0effc23710ab5a93
Dongbin <bnitech88@gmail.com>
2022-03-31 17:18:22 +0900

    [dongbin] #9.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SassComponent from './SassComponent';
 
 class App extends Component<any, any> {
   render() {
     return (
-      <div className="App">
-        \
-        <header className="App">
-          <img src={logo} className="logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            href="https://reactjs.org"
-            target="_blank"
-            rel="nooperner noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <SassComponent />
       </div>
     );
   }
