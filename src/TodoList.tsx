59a4fb28cdee77b4561ad1b8d9a1a007a16f2192
Dongbin <bnitech88@gmail.com>
2022-04-16 20:14:12 +0900

    [dongbin] #11.7

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,21 +1,29 @@
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 import Todo from './model/Todo';
+import React from 'react';
 
 interface MyProps {
   todos: Array<Todo>;
-  onRemove(id: number):void;
-  onToggle(id: number):void;
+
+  onRemove(id: number): void;
+
+  onToggle(id: number): void;
 }
 
 const TodoList = ({ todos, onRemove, onToggle }: MyProps) => {
   return (
     <div className="TodoList">
       {todos.map((todo: Todo) => (
-        <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle}/>
+        <TodoListItem
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
