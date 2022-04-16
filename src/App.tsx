e8b08f3d4a09b62538f2987ab7e12dd164afa537
Dongbin <bnitech88@gmail.com>
2022-04-16 16:45:44 +0900

    [dongbin] #10.2.3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import TodoTemplate from './TodoTemplate';
 import TodoInsert from './TodoInsert';
+import TodoList from './TodoList';
 
 const App = (): any => {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 };
