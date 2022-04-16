1a8bfcea93f2cdd0a78a743ef8758f71f79e561b
Dongbin <bnitech88@gmail.com>
2022-04-16 21:18:55 +0900

    [dongbin] #11.8.3

--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -7,6 +7,7 @@ import cn from 'classnames';
 import './TodoListItem.scss';
 import Todo from './model/Todo';
 import React from 'react';
+import CSS from 'csstype';
 
 interface MyProps {
   todo: Todo;
@@ -14,19 +15,26 @@ interface MyProps {
   onRemove(id: number): void;
 
   onToggle(id: number): void;
+
+  style: CSS.Properties;
 }
 
-const TodoListItem = ({ todo, onRemove, onToggle }: MyProps) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }: MyProps) => {
   const { id, text, checked } = todo;
 
   return (
-    <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove" onClick={() => onRemove(id)}>
-        <MdRemoveCircleOutline />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn('checkbox', { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
