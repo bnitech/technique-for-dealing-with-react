37b827edafab5a32a443001d3bca038061ac6e36
Dongbin <bnitech88@gmail.com>
2022-04-16 17:09:02 +0900

    [dongbin] #10.3.2

--- src/TodoInsert.tsx
+++ src/TodoInsert.tsx
@@ -1,10 +1,21 @@
 import { MdAdd } from 'react-icons/md';
+import { useCallback, useState } from 'react';
 import './TodoInsert.scss';
 
 const TodoInsert: any = () => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
   return (
     <form className="TodoInsert">
-      <input placeholder="할 일을 입력하세요" />
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
