7cfb69af7702626664dea1b71d638f41db595c7f
Dongbin <bnitech88@gmail.com>
2022-04-23 20:41:15 +0900

    [dongbin] #13.4.2

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,15 @@
+import { useLocation } from 'react-router-dom';
+import { Path } from 'history';
+
 const About = () => {
+  const location: Path = useLocation();
   return (
     <div>
       <h1>소개</h1>
       <p>리액트 라우터를 사용해 보는 프로젝트입니다.</p>
+      <p>쿼리스트링: {location.search}</p>
     </div>
-  )
-}
+  );
+};
 
 export default About;
