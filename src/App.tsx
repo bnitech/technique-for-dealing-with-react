d9667a93896fe5e3ba7494cfd590e466465ee41f
Dongbin <bnitech88@gmail.com>
2022-04-23 18:49:10 +0900

    [dongbin] #13.3.4

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,15 @@
 import './App.css';
-import React, { useCallback, useRef, useState } from 'react';
-import produce from 'immer';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
-interface MyInfo {
-  id: number;
-  name: string;
-  username: string;
-}
-
-const App = (): any => {
-  const nextId = useRef(1);
-  const [form, setForm] = useState({ name: '', username: '' });
-  const [data, setData] = useState({
-    array: new Array<MyInfo>(),
-    uselessValue: null,
-  });
-
-  const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setForm(
-      produce<any>((draft: any) => {
-        draft[name] = value;
-      }),
-    );
-  }, []);
-
-  const onSubmit = useCallback(
-    (e) => {
-      e.preventDefault();
-      const info: MyInfo = {
-        id: nextId.current,
-        name: form.name,
-        username: form.username,
-      };
-
-      setData(
-        produce((draft) => {
-          draft.array.push(info);
-        }),
-      );
-
-      setForm({
-        name: '',
-        username: '',
-      });
-      nextId.current += 1;
-    },
-    [form.name, form.username],
-  );
-
-  const onRemove = useCallback((id) => {
-    setData(
-      produce((draft) => {
-        draft.array.splice(
-          draft.array.findIndex((info) => info.id === id),
-          1,
-        );
-      }),
-    );
-  }, []);
-
+const App = () => {
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          name="username"
-          placeholder="아이디"
-          value={form.username}
-          onChange={onChange}
-        />
-        <input
-          name="name"
-          placeholder="이름"
-          value={form.name}
-          onChange={onChange}
-        />
-        <button type="submit">등록</button>
-      </form>
-      <div>
-        <ul>
-          {data.array.map((info) => (
-            <li key={info.id} onClick={() => onRemove(info.id)}>
-              {info.username} ({info.name})
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 };
 
