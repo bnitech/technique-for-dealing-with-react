e7395902a84fc822e7249bacf1f4c8ce983f882f
Dongbin <bnitech88@gmail.com>
2022-04-24 10:03:03 +0900

    [dongbin] #13.6.2

--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -2,40 +2,32 @@ import { NavLink, Outlet } from 'react-router-dom';
 import CSS from 'csstype';
 
 const Articles = () => {
+  return (
+    <div>
+      <Outlet />
+      <ul>
+        <ArticleItem id={1} />
+        <ArticleItem id={2} />
+        <ArticleItem id={3} />
+      </ul>
+    </div>
+  );
+};
+
+const ArticleItem = ({ id }: any) => {
   const activeStyle: CSS.Properties = {
     color: 'green',
     fontSize: '21px',
   };
   return (
-    <div>
-      <Outlet />
-      <ul>
-        <li>
-          <NavLink
-            to="/articles/1"
-            style={({ isActive }) => (isActive ? activeStyle : {})}
-          >
-            게시글 1
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/articles/2"
-            style={({ isActive }) => (isActive ? activeStyle : {})}
-          >
-            게시글 2
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/articles/3"
-            style={({ isActive }) => (isActive ? activeStyle : {})}
-          >
-            게시글 3
-          </NavLink>
-        </li>
-      </ul>
-    </div>
+    <li>
+      <NavLink
+        to={`/articles/${id}`}
+        style={({ isActive }) => (isActive ? activeStyle : {})}
+      >
+        게시글 {id}
+      </NavLink>
+    </li>
   );
 };
 
