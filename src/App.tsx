bca08944ea2cf9f662f3390c4b0e3c37dca8b6b0
Dongbin <bnitech88@gmail.com>
2022-04-24 09:07:50 +0900

    [dongbin] #13.5.1

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,16 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import Articles from './pages/Articles';
 import Article from './pages/Article';
+import Layout from "./layout";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profiles/:username" element={<Profile />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+      </Route>
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />
       </Route>
