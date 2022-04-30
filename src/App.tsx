1f644d20597aff70c556eaf8b720d9c11939b7f9
Dongbin <bnitech88@gmail.com>
2022-04-30 12:25:20 +0900

    [dongbin] #14.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
 import './App.css';
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Profile from './pages/Profile';
-import Articles from './pages/Articles';
-import Article from './pages/Article';
-import Layout from './layout';
-import NotFound from './pages/NotFound';
-import Login from './pages/Login';
-import MyPage from './pages/MyPage';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => {
+        setData(response.data);
+      });
+  };
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profiles/:username" element={<Profile />} />
-      </Route>
-      <Route path="/articles" element={<Articles />}>
-        <Route path=":id" element={<Article />} />
-      </Route>
-      <Route path="/login" element={<Login />} />
-      <Route path="/mypage" element={<MyPage />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
   );
 };
 
