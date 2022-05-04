9ae637b6a8e5424faa5e493aa848ac5e85c57c2f
Dongbin <bnitech88@gmail.com>
2022-05-04 21:24:46 +0900

    [dongbin] #15.4.2

--- src/components/SelectColors.tsx
+++ src/components/SelectColors.tsx
@@ -1,37 +1,54 @@
-import { ColorConsumer } from '../contexts/color';
-import {Color} from "../model/Color";
+import { Color } from '../model/Color';
+import React, { Component } from 'react';
+import ColorContext from '../contexts/color';
 
-const colors: Array<Color> = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const colors: Array<Color> = [
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'indigo',
+  'violet',
+];
 
-const SelectColors = () => {
-  return (
-    <div>
-      <h2>색상을 선택하세요.</h2>
-      <ColorConsumer>
-        {({ actions }) => (
-          <div style={{ display: 'flex' }}>
-            {colors.map((color) => (
-              <div
-                key={color}
-                style={{
-                  background: color,
-                  width: '24px',
-                  height: '24px',
-                  cursor: 'pointer',
-                }}
-                onClick={() => actions.setColor(color)}
-                onContextMenu={(e) => {
-                  e.preventDefault();
-                  actions.setSubColor(color);
-                }}
-              />
-            ))}
-          </div>
-        )}
-      </ColorConsumer>
-      <hr />
-    </div>
-  );
-};
+class SelectColors extends Component {
+  static contextType = ColorContext;
+
+  handleSetColor = (color: Color) => {
+    this.context.actions.setColor(color);
+  };
+
+  handleSetSubColor = (subColor: Color) => {
+    this.context.actions.setSubColor(subColor);
+  };
+
+  render(): React.ReactNode {
+    return (
+      <div>
+        <h2>색상을 선택하세요.</h2>
+        <div style={{ display: 'flex' }}>
+          {colors.map((color) => (
+            <div
+              key={color}
+              style={{
+                background: color,
+                width: '24px',
+                height: '24px',
+                cursor: 'pointer',
+              }}
+              onClick={() => this.handleSetColor(color)}
+              onContextMenu={(e) => {
+                e.preventDefault();
+                this.handleSetSubColor(color);
+              }}
+            />
+          ))}
+        </div>
+        <hr />
+      </div>
+    );
+  }
+}
 
 export default SelectColors;
