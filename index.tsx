d896e828290f437fc09c4dcd3f57b0f916da6d38
Dongbin <bnitech88@gmail.com>
2022-05-04 22:44:41 +0900

    [dongbin] #16.2.5

--- index.tsx
+++ index.tsx
@@ -10,3 +10,8 @@ const DECREASE = 'DECREASE';
 const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
 const increase = (difference: any) => ({ type: INCREASE, difference });
 const decrease = () => ({ type: DECREASE });
+
+const initialSate = {
+  toggle: false,
+  counter: 0
+}
