a36088cc730c939c0444edef4e121e46fbb6bee9
Dongbin <bnitech88@gmail.com>
2022-05-04 22:54:06 +0900

    [dongbin] #16.2.7

--- index.tsx
+++ index.tsx
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const divToggle = document.querySelector('.toggle');
 const counter = document.querySelector('h1');
 const btnIncrease = document.querySelector('#increase');
@@ -7,33 +9,35 @@ const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
 const INCREASE = 'INCREASE';
 const DECREASE = 'DECREASE';
 
-const toggleSwitch = () => ({type: TOGGLE_SWITCH});
-const increase = (difference: any) => ({type: INCREASE, difference});
-const decrease = () => ({type: DECREASE});
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = (difference: any) => ({ type: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
 
 const initialSate = {
   toggle: false,
-  counter: 0
-}
+  counter: 0,
+};
 
 function reducer(state = initialSate, action) {
   switch (action.type) {
     case TOGGLE_SWITCH:
       return {
         ...state,
-        toggle: !state.toggle
-      }
+        toggle: !state.toggle,
+      };
     case INCREASE:
       return {
         ...state,
-        counter: state.counter + action.difference
-      }
+        counter: state.counter + action.difference,
+      };
     case DECREASE:
       return {
         ...state,
-        counter: state.counter - 1
-      }
+        counter: state.counter - 1,
+      };
     default:
       return state;
   }
 }
+
+const store = createStore(reducer);
