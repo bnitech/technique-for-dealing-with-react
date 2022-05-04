55b5daa5cbefb3b6cdfc7e26b0d7c54962065a12
Dongbin <bnitech88@gmail.com>
2022-05-04 20:04:32 +0900

    [dongbin] #15.3.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
 import React from 'react';
 import ColorBox from './components/ColorBox';
+import ColorContext from './contexts/color';
 
 const App = () => {
   return (
-    <div>
-      <ColorBox />
-    </div>
+    <ColorContext.Provider value={{ color: 'red' }}>
+      <div>
+        <ColorBox />
+      </div>
+    </ColorContext.Provider>
   );
 };
 
--- src/contexts/color.tsx
+++ src/contexts/color.tsx
@@ -1,5 +1,47 @@
-import { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
-const ColorContext = createContext({ color: 'black' });
+type Color = 'black' | 'red';
+
+interface State {
+  color: Color;
+  subColor: Color;
+}
+
+interface Value {
+  state: State;
+  actions: {
+    setColor(color: Color): void;
+    setSubColor(color: Color): void;
+  };
+}
+
+const ColorContext = createContext<Value | null>({
+  state: { color: 'black', subColor: 'red' },
+  actions: {
+    setColor: () => {
+      return;
+    },
+    setSubColor: () => {
+      return;
+    },
+  },
+});
+
+const ColorProvider = ({ children }: { children: React.ReactNode }) => {
+  const [color, setColor] = useState<Color>('black');
+  const [subColor, setSubColor] = useState<Color>('red');
+
+  const value: Value = {
+    state: { color, subColor },
+    actions: { setColor, setSubColor },
+  };
+  return (
+    <ColorContext.Provider value={value}>{children}</ColorContext.Provider>
+  );
+};
+
+const { Consumer: ColorConsumer } = ColorContext;
+
+export { ColorProvider, ColorConsumer };
 
 export default ColorContext;
