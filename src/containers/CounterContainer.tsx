590537f85c5d14ed1ac4f7aac7c188aa38497da3
Dongbin <bnitech88@gmail.com>
2022-05-07 01:01:34 +0900

    [dongbin] #17.7.2

--- src/containers/CounterContainer.tsx
+++ src/containers/CounterContainer.tsx
@@ -1,16 +1,15 @@
 import Counter from '../components/Counters';
 import { useDispatch, useSelector } from 'react-redux';
 import { decrease, increase } from '../modules/counter';
+import { useCallback } from 'react';
 
 const CounterContainer = () => {
   const number = useSelector((state: any) => state.counter.number);
   const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
   return (
-    <Counter
-      number={number}
-      onIncrease={() => dispatch(increase())}
-      onDecrease={() => dispatch(decrease())}
-    />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
 
