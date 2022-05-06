f85052aa84ad581e270330fb6dfc7115bcd848be
Dongbin <bnitech88@gmail.com>
2022-05-06 21:03:11 +0900

    [dongbin] #17.3.2.2

--- src/modules/todos.tsx
+++ src/modules/todos.tsx
@@ -2,3 +2,28 @@ const CHANGE_INPUT = 'todos/CHANGE_INPUT';
 const INSERT = 'todos/INSERT';
 const TOGGLE = 'todos/TOGGLE';
 const REMOVE = 'todos/REMOVE';
+
+export const changeInput = (input: string) => ({
+  type: CHANGE_INPUT,
+  input,
+});
+
+let id = 3;
+export const insert = (text: string) => ({
+  type: INSERT,
+  todo: {
+    id: id++,
+    text,
+    done: false,
+  },
+});
+
+export const toggle = (id: number) => ({
+  type: TOGGLE,
+  id,
+});
+
+export const remove = (id: number) => ({
+  type: REMOVE,
+  id,
+});
