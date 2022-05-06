be9fc44028306350c37e1ef694ae79216a378a98
Dongbin <bnitech88@gmail.com>
2022-05-07 00:55:31 +0900

    [dongbin] #17.7.1

--- src/containers/CounterContainer.tsx
+++ src/containers/CounterContainer.tsx
@@ -1,27 +1,12 @@
 import Counter from '../components/Counters';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { decrease, increase } from '../modules/counter';
 
-interface MyProps {
-  number: number;
-
-  increase(): void;
-
-  decrease(): void;
-}
-
-const CounterContainer = ({ number, increase, decrease }: MyProps) => {
+const CounterContainer = () => {
+  const number = useSelector((state: any) => state.counter.number);
   return (
     <Counter number={number} onIncrease={increase} onDecrease={decrease} />
   );
 };
 
-export default connect(
-  (state: any) => ({
-    number: state.counter.number,
-  }),
-  {
-    increase,
-    decrease,
-  },
-)(CounterContainer);
+export default CounterContainer;
