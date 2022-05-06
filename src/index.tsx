7869e1e67af2b97d069635fb5c7e9cc8fd134f7c
Dongbin <bnitech88@gmail.com>
2022-05-06 21:48:46 +0900

    [dongbin] #17.4.2

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,13 @@ import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import rootReducer from './modules';
+import { Provider } from 'react-redux';
 
 const store = createStore(rootReducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
