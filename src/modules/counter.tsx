c0c8cf76ff037b97d5a2de5f28c6fc55c0a98379
Dongbin <bnitech88@gmail.com>
2022-05-06 20:56:24 +0900

    [dongbin] #17.3.1.3

--- src/modules/counter.tsx
+++ src/modules/counter.tsx
@@ -1,5 +1,38 @@
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+export const increase = (): Action => ({ type: INCREASE });
+export const decrease = (): Action => ({ type: DECREASE });
+
+interface State {
+  number: number;
+  menubar: number;
+}
+
+interface Action {
+  type: string;
+}
+
+const initialSate: State = {
+  number: 0,
+  menubar: 0,
+};
+
+function counter(state = initialSate, action: Action): State {
+  switch (action.type) {
+    case INCREASE:
+      return {
+        ...state,
+        ['menubar']: state.number + 1,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        ['menubar']: state.number - 1,
+      };
+    default:
+      return state;
+  }
+}
+
+export default counter;
