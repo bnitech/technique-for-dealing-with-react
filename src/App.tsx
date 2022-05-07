8374da9ebe454fb0e1e195feb6a8dc92d708172d
Dongbin <bnitech88@gmail.com>
2022-05-08 00:49:05 +0900

    [dongbin] #18.3.2.5

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css';
 import React from 'react';
-import SampleContainer from './containers/SampleContainer';
+import CounterContainer from './containers/CounterContainer';
 
 const App = () => {
   return (
     <div>
-      <SampleContainer />
+      <CounterContainer />
     </div>
   );
 };
