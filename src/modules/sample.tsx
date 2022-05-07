895013cae2a9d6014e4f06b52bbfe10005e03b12
Dongbin <bnitech88@gmail.com>
2022-05-08 00:39:52 +0900

    [dongbin] #18.3.2.3

--- src/modules/sample.tsx
+++ src/modules/sample.tsx
@@ -1,6 +1,8 @@
-import { handleActions } from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
 import * as api from '../lib/api';
-import createRequestThunk from '../lib/createRequestThunk';
+import { finishLoading, startLoading } from './loading';
+import { call, put, takeLatest } from 'redux-saga/effects';
+import { AxiosResponse } from 'axios';
 
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
@@ -10,8 +12,49 @@ const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
 const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
-export const getPost = createRequestThunk(GET_POST, api.getPost);
-export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+export const getPost = createAction(GET_POST, (id: number) => id);
+export const getUsers = createAction(GET_USERS);
+
+function* getPostSaga(action: any) {
+  yield put(startLoading(GET_POST));
+  try {
+    const post: AxiosResponse = yield call(api.getPost, action.payload);
+    yield put({
+      type: GET_POST_SUCCESS,
+      payload: post.data,
+    });
+  } catch (e) {
+    yield put({
+      type: GET_POST_FAILURE,
+      payload: e,
+      error: true,
+    });
+  }
+  yield put(finishLoading(GET_POST));
+}
+
+function* getUserSaga() {
+  yield put(startLoading(GET_USERS));
+  try {
+    const users: AxiosResponse = yield call(api.getUsers);
+    yield put({
+      type: GET_USERS_SUCCESS,
+      payload: users.data,
+    });
+  } catch (e) {
+    yield put({
+      type: GET_USERS_FAILURE,
+      payload: e,
+      error: true,
+    });
+  }
+  yield put(finishLoading(GET_USERS));
+}
+
+export function* sampleSage() {
+  yield takeLatest(GET_POST, getPostSaga);
+  yield takeLatest(GET_USERS, getUserSaga);
+}
 
 const initialState = {
   post: null,
