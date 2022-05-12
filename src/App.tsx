624009a8b442181f092cf6e23dbe2a18583e5c88
Dongbin <bnitech88@gmail.com>
2022-05-12 20:37:41 +0900

    [dongbin] #19.2.3

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import './App.css';
-import React, { Suspense, useState } from 'react';
-const SplitMe = React.lazy(() => import('./SplitMe'));
+import React, { useState } from 'react';
+import loadable from '@loadable/component';
+
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>loading...</div>,
+});
 
 function App() {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
   };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <p onClick={onClick}>Hello React!</p>
-        <Suspense fallback={<div>loading...</div>}>
-          {visible && <SplitMe />}
-        </Suspense>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React!
+        </p>
+        {visible && <SplitMe />}
       </header>
     </div>
   );
