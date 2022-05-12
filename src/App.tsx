ca00b108aa1c49c7a050d2e815101b9d9864b349
Dongbin <bnitech88@gmail.com>
2022-05-12 19:45:52 +0900

    [dongbin] #19.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import notify from './notify';
 
-const App = () => {
+function App() {
   const onClick = () => {
-    notify();
+    import('./notify').then((result) => result.default());
   };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <p onClick={onClick}>Hello React!</p>
       </header>
     </div>
   );
-};
+}
 
 export default App;
