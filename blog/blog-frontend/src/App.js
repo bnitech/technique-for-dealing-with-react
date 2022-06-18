758d33358016c11a0896c9eb8b85ba5bfe37546d
Dongbin <bnitech88@gmail.com>
2022-06-19 08:26:34 +0900

    [dongbin] #24.1.2

--- blog/blog-frontend/src/App.js
+++ blog/blog-frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import PostListPage from './pages/PostListPage.tsx';
+import LoginPage from './pages/LoginPage.tsx';
+import RegisterPage from './pages/RegisterPage.tsx';
+import WritePage from './pages/WritePage.tsx';
+import PostPage from './pages/PostPage.tsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<PostListPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/write" element={<WritePage />} />
+      <Route path="/@:username">
+        <Route index element={<PostListPage />} />
+        <Route path=":posetId" element={<PostPage />} />
+      </Route>
+    </Routes>
   );
 }
 
