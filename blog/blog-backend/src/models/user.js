03133dea482c2bf48c9340a6959b0dadd6502a51
Dongbin <bnitech88@gmail.com>
2022-06-19 05:42:44 +0900

    [dongbin] #23.2.1

--- blog/blog-backend/src/models/user.js
+++ blog/blog-backend/src/models/user.js
@@ -1,9 +1,20 @@
-import mongoose, {Schema} from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
+import bcrpt from 'bcrypt';
 
 const UserSchema = new Schema({
   username: String,
-  hashedPassword: String
+  hashedPassword: String,
 });
+
+UserSchema.methods.setPassword = async function (password) {
+  const hash = await bcrpt.hash(password, 10);
+  this.hashedPassword = hash;
+};
+
+UserSchema.methods.checkPassword = async function (password) {
+  const result = await bcrpt.compare(password, this.hashedPassword);
+  return result;
+};
 
 const User = mongoose.model('User', UserSchema);
 export default User;
